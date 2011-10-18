fe33c14b95ead677537f8dbc13fbd60e0bb1dda4
r888888888 <r888888888@gmail.com>
2011-10-18 12:59:42 -0400

    removed run_meimei

--- run_meimei.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'rubygems'
-require 'meimei'
-
-client = Meimei::Client.new("meimei", :password => "mercury555lampe", :log_dir => "/home/albert/meimei/log")
-client.add_server("irc.synirc.net", 6667, "#meimei,#marimite,#circlenine,#raspberryheaven")
-#client.add_server("irc.synirc.net", 6667, "#meimei")
-client.start()
