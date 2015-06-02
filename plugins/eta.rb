4337333aff2c70a4ecce85abcd757da1714a0857
Albert Yi <ayi@desk.com>
2015-06-01 17:40:46 -0700

    update !eta plugin to use anime.yshi.org

--- plugins/eta.rb
+++ plugins/eta.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'net/http'
-require 'nokogiri'
+require 'json'
+require 'damerau-levenshtein'
+require 'time_difference'
 
 define_plugin("!eta") do |msg|
   query = msg
@@ -8,36 +10,33 @@ define_plugin("!eta") do |msg|
   if query.strip == ""
     reply "http://www.mahou.org/Showtime"
   else
-    Net::HTTP.start("www.mahou.org", 80) do |http|
-      resp = http.get("/Showtime")
+    Net::HTTP.start("anime.yshi.org", 80) do |http|
+      resp = http.get("/api/calendar/upcoming/all")
       if resp.code !~ /200/
         reply "Could not fetch web page, try again later"
         break
       end
 
       answer = "Show not found"
-      doc = Nokogiri::HTML(resp.body)
-      doc.xpath('//table[@summary="Currently Airing"]/tr/td/table/tr').each do |element|
-        name = element.children[2].text
-        eta = element.children[12].text
-        if name =~ /#{query}/i
-          answer = "ETA for #{name}: #{eta}"
-          break
+      json = JSON.parse(resp.body)
+      best, score = nil, 1_000
+      json.each do |show|
+        d = DamerauLevenshtein.distance(show["title_name"].downcase, query)
+        if d < score
+          best = show
+          score = d
         end
       end
 
-      if answer == "Show not found"
-        doc.xpath('//table[@summary="Starting Soon"]/tr/td/table/tr').each do |element|
-          name = element.children[2].text
-          eta = element.children[16].text
-          if name =~ /#{query}/i
-            answer = "ETA for #{name}: #{eta}"
-            break
-          end
-        end
-      end
+      if best
+        anime = best["title_name"]
+        episode = best["count"]
+        start_time = Time.at(best["start_time"].to_i)
+        distance = TimeDifference.between(start_time, Time.now).in_general
+        distance_s = "#{distance[:days]}d #{distance[:hours]}h #{distance[:minutes]}m"
 
-      reply answer
+        reply "ETA for #{anime} (ep #{episode}): #{distance_s}"
+      end
     end
   end
 end
