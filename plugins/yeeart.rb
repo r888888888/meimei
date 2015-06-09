7bee458f0c8bc7e2b81d664611f765daadb8f571
Albert Yi <ayi@desk.com>
2015-06-09 12:53:27 -0700

    improve yeeart

--- plugins/yeeart.rb
+++ plugins/yeeart.rb
@@ -1,6 +1,10 @@
 define_plugin("yeeart") do |msg|
-  next unless msg =~ /\by+e(e+)a+r+t+\b/i
-  size = [$1.size, 400].min
-  resp = "YE" + ("E" * (size + 1)) + "ART!"
+  next unless msg =~ /\b(y+)(ee+)(a+)(r+)(t+)\b/i
+  ys = [$1.size, 50].min
+  es = [$2.size, 300].min
+  as = [$3.size, 50].min
+  rs = [$4.size, 50].min
+  ts = [$5.size, 50].min
+  resp = ("Y" * (ys + 1)) + ("E" * (es + 1)) + ("A" * (as + 1)) + ("R" * (rs + 1)) + ("T" * (ts + 1)) + "!"
   reply resp
 end
